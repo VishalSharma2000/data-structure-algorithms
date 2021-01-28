a65f2a70d3775e42c3ad9505410544343931b156
vishal <vishaljhsharma@gmail.com>
2021-01-28 19:32:29 +0530

    Added Left View of binary Tree

---
+++ cpp/Trees/LeftView.cpp
@@ -0,0 +1,22 @@
+vector<int> leftView(Node *root)
+{
+    if(!root) return {};
+    
+    vector<int> ans;
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()) {
+        int size = q.size();
+        for(int i=0; i<size; i++) {
+            Node *temp = q.front();
+            q.pop();
+            
+            if(i == 0) ans.push_back(temp->data);
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+    
+    return ans;
+}
