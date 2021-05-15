6c25f39dd231e0162cb6a62c63fff8141b97748c
vishal <vishaljhsharma@gmail.com>
2021-05-15 11:22:00 +0530

    Added Longest Common SuperSequence

---
+++ cpp/Dynamic Programming/SCS.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS(string s1, string s2, int n, int m) {
+  int dp[n+1][m+1];
+
+  for(int i=0; i<n+1; i++) dp[i][0] = 0;
+  for(int i=0; i<m+1; i++) dp[0][i] = 0;
+
+  for(int i=1; i<n+1; i++) {
+    for(int j=1; j<m+1; j++) {
+      if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+      else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    }
+  }
+
+  return dp[n][m];
+}
+
+int lenSCS(string s1, string s2, int n, int m) {
+  int len = LCS(s1, s2, n, m);
+
+  /* Which all characters are common in both string, for finding the shortest supersequence we should write those
+  characters only once. And also one thinking is that we should arrange the all characters in such an order that the length
+  decreases...so note that those set of characters for which proper ordering will result into less supersequence length
+  is the Longest common subsequence. 
+  
+  So, in the final string we should write the longest common supersequence only once.  */
+
+  return (m + n) - (len);
+} 
+
+int main() {
+  string s1, s2;
+  cin >> s1 >> s2;
+
+  cout << lenSCS(s1, s2, s1.length(), s2.length()) << endl;
+}
