2884b30f8d84b2f0b0157c2d31db2e828e49ceec
vishal <vishaljhsharma@gmail.com>
2021-05-20 10:44:58 +0530

    Added Recursive approach for reversing the linked list

--- cpp/Linked List/ReverseList.cpp
+++ cpp/Linked List/ReverseList.cpp
@@ -1,27 +1,90 @@
-ListNode* reverseList(ListNode* head) {
-        if(!head || (!head->next))  return head;
-        
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-        
-        while(temp) {
-            ListNode *temp1 = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = temp1;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node(int val) {
+            data = val;
+            next = NULL;
         }
-        
-        return prev;
+};
+
+class LinkedList {
+    public:
+        Node *head; // for pointing the first node of the linked list
+        Node *tail; // for pointing the last node of the linked list
+
+        LinkedList() {
+            head = NULL;
+        }
+
+        Node* reverseUtil(Node *head) {
+            if(head == NULL || head->next == NULL) return head;
+
+            // dividing linked list into two parts - first node and rest part
+            Node *rest = reverseUtil(head->next);
+            /* When the above recursive function ends for first time, then we will be at the second last node of the of the linked list 
+            Basically, we are reversing the linked list from last...
+            */
+            head->next->next = head;
+            head->next = NULL;
+
+            return rest;
+        }
+
+        void reverseLL() {
+            tail = head;
+            head = reverseUtil(head);
+        }
+
+        void print() {
+            Node *temp = head;
+            while(temp != NULL) {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+
+            return;
+        }
+
+        void push(int val) {
+            Node *newNode = new Node(val);
+            if(head == NULL) {
+                head = newNode;
+                tail = newNode;
+            }
+
+            tail->next = newNode;
+            tail = newNode;
+        }
+};
+
+int main() {
+    LinkedList ll;
+    for(int i=1; i<=6; i++) ll.push(i * 10);
+
+    ll.print();
+    ll.reverseLL();
+    ll.print();
 }
 
-// recursive approach
-ListNode* reverseList(ListNode *prev, ListNode *temp) {
-        if(temp->next == NULL) {
-            temp->next = prev;
-            return temp;
-        }
+// ListNode* reverseList(ListNode* head) {
+//         if(!head || (!head->next))  return head;
         
-        ListNode *temp1 = reverseList(temp, temp->next);
-        temp->next = prev;
-        return temp1;
-}
+//         ListNode* temp = head;
+//         ListNode* prev = NULL;
+        
+//         while(temp) {
+//             ListNode *temp1 = temp->next;
+//             temp->next = prev;
+//             prev = temp;
+//             temp = temp1;
+//         }
+        
+//         return prev;
+// }
+
