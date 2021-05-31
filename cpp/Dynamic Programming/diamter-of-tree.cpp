f5a144febabbd1ed010e46fe01678d37e2c9a646
Vishal Sharma <42766370+VishalSharma2000@users.noreply.github.com>
2021-05-31 14:58:45 +0530

    Added Diameter of the tree

---
+++ cpp/Dynamic Programming/diamter-of-tree.cpp
@@ -0,0 +1,34 @@
+int solve(TreeNode *root, int &res) {
+        /* Base Condition */
+        if(!root) return 0;
+        
+        /* Hypothesis */
+        int left = solve(root->left, res);
+        int right = solve(root->right, res);
+        
+        /* Induction */
+        /* 
+            temp => if current node is not considered as the root of the path then we should return the maximum height of the tree till current node so that
+            it can be used to find the diameter for the ancestor nodes
+            ans => if current node is considered as the root of the path then the length of the diameter will be sum of height of left sub tree and right sub tree
+        */
+        
+        /* 1 is added for the edge which is connection the left or the right sub tree with the current node
+        Note that we used to add another base condition like if(!root->left && !root->right) return 0; if the measurement is done based on the edge and not on the
+        node. But here it is not working because in that case we treat the null value and the leaf node at the same height. */
+        int temp = max(left, right) + 1;
+        int ans = left + right;
+  
+        /* Keeping track of the final answer */
+        res = max(res, ans);
+        
+        /* Indirectly, Returning the maximum height of the tree till now */
+        return temp;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = INT_MIN;
+        solve(root, res);
+        
+        return res;
+    }
