5c855b997ffd8110d9eac684d2e951e72bd8c7cb
Vishal Sharma <42766370+VishalSharma2000@users.noreply.github.com>
2021-07-03 11:55:13 +0530

    Update: k closest elements
    Added another solution for k closest elements

--- Final 450 Questions/cpp/Heap/k-closest-elements.cpp
+++ Final 450 Questions/cpp/Heap/k-closest-elements.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<pair<int, int> > temp;
+        
+        for(int i=0; i<(int) arr.size(); i++) {
+            temp.push_back({arr[i], abs(arr[i] - x)});
+        }
+        
+        sort(temp.begin(), temp.end(), [](pair<int, int> &p1, pair<int, int> &p2) {
+            if(p1.second == p2.second) return p1.first < p2.first;
+            return p1.second < p2.second;
+        });
+        
+        vector<int> ans;
+        for(int i=0; i<k; i++) {
+            ans.push_back(temp[i].first);
+        }
+        
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         priority_queue<pair<int, int>> maxHeap;
         
         for(int i=0; i<arr.size(); i++) {
