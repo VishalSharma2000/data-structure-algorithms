fe4e0014f7d432bef1447702c96768f78a72d7bf
vishal <vishaljhsharma@gmail.com>
2021-08-05 16:16:59 +0530

    Update Kth Smallest Element in distinct array

--- Final 450 Questions/cpp/Arrays/kth-smallest-element-in-distinct-array.cpp
+++ Final 450 Questions/cpp/Arrays/kth-smallest-element-in-distinct-array.cpp
@@ -2,6 +2,11 @@
 
 /* Here we are doing Quick Sort, which is basically an optimization of sorting approach(Time Complexity: O(NLogN)) so here the main idea is to sort the array only till kth position and whenever we encounter a pivot at (k-1)th position then we can return the element at that position */
 /* This method will work only when the array elements are distinct bcoz in this case we are assured that the kth smallest element will be at (k-1)th index of the array but if the array elements are not distinct then this approach will give wrong answer */
+
+/* We can also use randomized quick select. In randomized quick select worst case time complexity will be still O(N^2) and average case
+time complexity will O(N). We say that randomized quick select works better than normal quick select because the probability of getting
+a number from 0 to 9 is same to there is more chance that the array will be divided into two halves */
+
 /* Time Complexity: O(N) for average cases and O(N^2) for worst cases */
 
 int partition(vector<int> &arr, int l, int h) {
@@ -51,3 +56,25 @@ int main() {
     if(kthSmallest == INT_MAX) cout << "Kth smallest does not exist" << endl;
     else cout << kthSmallest << endl;
 }
+
+
+/* USING STL MAP */
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        map<int, int, greater<int>> mp;
+        
+        for(int i=0; i<nums.size(); i++) {
+            mp[nums[i]]++;
+        }
+        
+        int i = 0;
+        for(auto it=mp.begin(); it!=mp.end(); it++) {
+            i += it->second;
+            
+            if(i >= k) return it->first;
+        }
+        
+        return INT_MAX;
+    }
+};
