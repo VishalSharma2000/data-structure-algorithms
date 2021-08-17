7c3c22f2ed6651afb4b53ac7bc4ae88aaf6314db
vishal <vishaljhsharma@gmail.com>
2021-08-17 22:42:27 +0530

    Update Count good nodes

--- Leetcode/count-good-nodes-binary-tree.cpp
+++ Leetcode/count-good-nodes-binary-tree.cpp
@@ -1,6 +1,7 @@
 /* Given binary tree we have to find the number of good nodes. A node is called as good node when all the nodes in the path from root to that node has values less than the node. */
 
 /* DFS Approach - TC: O(N), SP: O(height of tree) */
+/* Trace the maximum value while traversing the node in dfs approach. For each node compare if the current node's value is more than equal to the maximum value till now. If the value is greater then it is good node otherwise not */
 class Solution {
 public:  
     int findGoodNodes(TreeNode *root, int maxNode) {
@@ -15,5 +16,35 @@ public:
     
     int goodNodes(TreeNode* root) {        
         return findGoodNodes(root, root->val);
+    }
+};
+
+/* BFS Approach - TC: O(N), SP: O(maximum no. of nodes in any level) */
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q;
+        
+        q.push({ root, INT_MIN });
+        int count = 0;
+        
+        while(!q.empty()) {
+            TreeNode *curr = q.front().first;
+            int prevMax = q.front().second;
+            q.pop();
+            
+            if(curr->val >= prevMax) {
+                count++;
+            }
+            
+            if(curr->left) {
+                q.push({ curr->left, max(prevMax, curr->val)});
+            }
+            if(curr->right) {
+                q.push({ curr->right, max(prevMax, curr->val)});
+            }
+        }
+        
+        return count;
     }
 };
