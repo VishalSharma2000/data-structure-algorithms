f67fed073e433ab974413c76d5327522146cf88b
vishal <vishaljhsharma@gmail.com>
2021-11-05 20:17:22 +0530

    Update Knapsack Problem

--- Final 450 Questions/cpp/Dynamic Programming/knapsack-prb-bottom-up.cpp
+++ Final 450 Questions/cpp/Dynamic Programming/knapsack-prb-bottom-up.cpp
@@ -1,39 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int knapsackBottomUp(int weights[], int profit[], int W, int n) {
-	int DP[n+1][W+1];
-
-	/* If the space in knapsack is 0 or the number of objects present is zero, then the maximum profit 
-	   which can be obtained is zero */
-	for(int i=0; i<n+1; i++) 
-		DP[i][0] = 0;
-	for(int i=0; i<W+1; i++) 
-		DP[0][i] = 0;
-
-
-	/*
-		Think it like this...initially you don't have any object with you to add into knapsack neither you have any space in knapsack
-		DP[n1-1][w1 - weights[n1]] => states that if we are going to add the object with index n1 into the existing object list, so we
-		will have to decrease the knapsack remaining space by weights[i] and then with the remaining space we will check which one is giving 
-		more profit.
-
-		The inner loop is checking for all weights from 1 to W, in recursive call we decrease the given weight with the weight of object
-		and then check for the other elements if they can be added or not...same way but here we are checking for all the weights from 
-		1 to W iteratively in the inner loop...
-	*/
-	for(int n1 = 1; n1 < n+1; n1++) {
-		for(int w1 = 1; w1 < W+1; w1++) {
-			if(weights[n1-1] <= w1)
-				DP[n1][w1] = max(profit[n1-1] + DP[n1-1][w1 - weights[n1-1]], DP[n1-1][w1]);
-			else
-				DP[n1][w1] = DP[n1-1][w1];
-		}
-	}
-
-
-	return DP[n][W];
-}
 
 int main() {
 	int n;
--- Final 450 Questions/cpp/Dynamic Programming/knapsack-problem.cpp
+++ Final 450 Questions/cpp/Dynamic Programming/knapsack-problem.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/* Recursive Approach Start */
+int KPRecursive(int weights[], int profit[], int W, int n) {
+  if(W == 0 || n == 0) return 0;
+  /* If no space left in the bag or their are no more elements to put in knapsack so now in this case the profit will be zero */
+
+  /* Here we are taking decision on the last element of the array. 
+  So, If the weight of the current element on which decision is being taken is less then the remaining space left in the bag then we have choice to either consider the element or don't consider the element otherwise if the current element's weight is more than available space then we can't consider that element. */
+
+  if(weights[n-1] <= W) {
+    int profitIfConsidered = profit[n-1] + KPRecursive(weights, profit, W - weights[n-1], n-1);
+    int profitIfNotConsidered = KPRecursive(weights, profit, W, n-1);
+
+    return max(profitIfConsidered, profitIfNotConsidered);
+  }
+
+  // If the weight of the current element is more than the remaining space then we can't consider the element
+  return KPRecursive(weights, profit, W, n-1);
+}
+/* Recursive Approach End */
+
+
+
+
+
+/* Memoization Approach Start */
 #define MAX 100
 
 int DP[MAX][MAX];
 
-int knapsackDP(int weights[], int profit[], int W, int n) {
+int KPMemoization(int weights[], int profit[], int W, int n) {
     // base condition of recursive calls
     if(n == 0 || W == 0) {
         /* if there are no elements to keep in knapsack or if there are no remaining space in the bag then the profit will be zero */
@@ -21,12 +46,47 @@ int knapsackDP(int weights[], int profit[], int W, int n) {
     if(DP[W][n] != -1) return DP[W][n];
     
     if(weights[n-1] <= W) {
-        return DP[W][n] = max(profit[n-1] + knapsackDP(weights, profit, W-weights[n-1], n-1), knapsackDP(weights, profit, W, n-1));
+        return DP[W][n] = max(
+          profit[n-1] + KPMemoization(weights, profit, W-weights[n-1], n-1), 
+          KPMemoization(weights, profit, W, n-1)
+        );
     } 
     
     // if the weight of the object at index n-1 is more that knapsack weight
-    return DP[W][n] = knapsackDP(weights, profit, W, n-1);
+    return DP[W][n] = KPMemoization(weights, profit, W, n-1);
 }
+/* Memoization Approach End */
+
+
+
+
+/* Top Down Approach Start */
+int KPTopDown(int weights[], int profit[], int W, int n) {
+	int DP[n+1][W+1];
+
+	/* If the space in knapsack is 0 or the number of objects present is zero, then the maximum profit 
+	   which can be obtained is zero */
+	for(int i=0; i<n+1; i++) 
+		DP[i][0] = 0;
+	for(int i=0; i<W+1; i++) 
+		DP[0][i] = 0;
+
+  /* At each iteration i we are checking if the ith element can be taken into consideration for all weight from 0 to W. If yes then we we find the max profit between (if object considered) or (if not considered), otherwise the maximum profit for the sub problem will be same as in the case when the object is not considered */
+	for(int i = 1; i < n+1; i++) {
+		for(int j = 1; j < W+1; j++) {
+			if(weights[i-1] <= j)
+        // subproblem(i, j) = max(curr_obj_profit + subproblem(i-1)(j - weight), subproblem(i-1, j))
+				DP[i][j] = max(profit[i-1] + DP[i-1][j - weights[i-1]], DP[i-1][j]);
+			else
+				DP[i][j] = DP[i-1][j];
+		}
+	}
+
+
+	return DP[n][W];
+}
+
+/* Top Down Approach End */
 
 int main() {
     int n;
@@ -36,7 +96,9 @@ int main() {
     for(int i=0; i<n; i++) cin >> weights[i];
     for(int i=0; i<n; i++) cin >> profit[i];
     cin >> W;
+
+    cout << "Recursive Solution: " << KPRecursive(weights, profit, W, n) << endl;
     
     memset(DP, -1, sizeof(DP));
-    cout << knapsackDP(weights, profit, W, n) << endl;
+    cout << KPMemoization(weights, profit, W, n) << endl;
 }
